41d3ece5c412ae4ea831a557dcdf2660c4cd7448
NeleSuffo <nele.suffo@leuphana.de>
2025-10-06 22:07:49 +0200

    Update image paths in run_deepface.py and enhance output messages for face verification results

--- run_deepface.py
+++ run_deepface.py
@@ -1,7 +1,10 @@
 from deepface import DeepFace
 
-img1_path = "/home/nele_pauline_suffo/outputs/face_detections/quantex_at_home_id255237_2022_05_08_03_051960_face_1.png"
-img2_path = "/home/nele_pauline_suffo/outputs/face_detections/quantex_at_home_id257147_2021_10_31_03_000670_face_1.png"
+img1_path = "/home/nele_pauline_suffo/outputs/face_detections/quantex_at_home_id255944_2022_03_08_01_annotated/groups/person_3/quantex_at_home_id255944_2022_03_08_01_011530_face_1.PNG"
+img2_path = "/home/nele_pauline_suffo/outputs/face_detections/quantex_at_home_id255944_2022_03_08_01_annotated/groups/person_4/quantex_at_home_id255944_2022_03_08_01_011650_face_1.PNG"
 result = DeepFace.verify(img1_path = img1_path, img2_path = img2_path, enforce_detection=False)
 
-print(result)
+if result["verified"]:
+    print("Faces match!")
+else:
+    print("Faces do not match.")
